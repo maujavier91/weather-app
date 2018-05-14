1f3979bbf93d32fd3657ccd973f773531d2a7daf
Mauricio Naranjo <maujavier91@gmail.com>
2018-05-14 15:02:48 +0000

    Added success and error callbacks to getCurrentPosition() Tested hardcoded fetch api and freecodecamp weather endpoint

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-let location = document.getElementById("location");
+let location_ = document.getElementById("location");
 let weather = document.getElementById("weather");
 let units = document.getElementById("units");
 let settings = {};
 let endpoint_url = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139" 
 
-let currentLocation = navigator.geolocation.getCurrentPosition();
+let currentLocation = navigator.geolocation.getCurrentPosition(
+    pos => {
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);},
+    error=>console.log(error));
 
 fetch(endpoint_url, {method: "GET", mode: "cors", }).then(res => res.json()).then(function(data){
-    console.log(data);
+  console.log(data);
 });
 
