ebb8ed167dc807a016ece27f9425b4e14b87de28
Mauricio <maujavier91@gmail.com>
2018-05-14 10:33:28 -0400

    Test location api and fetch api

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let location = document.getElementById("location");
 let weather = document.getElementById("weather");
-let units = document.getElementById("units")
+let units = document.getElementById("units");
+let settings = {};
+let endpoint_url = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139" 
+
+let currentLocation = navigator.geolocation.getCurrentPosition();
+
+fetch(endpoint_url, {method: "GET", mode: "cors", }).then(res => res.json()).then(function(data){
+    console.log(data);
+});
+
